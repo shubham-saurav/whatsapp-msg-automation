684049236a18b7b76c7257338379e2d6e174b309
shubham-saurav <53931617+shubham-saurav@users.noreply.github.com>
2020-10-07 19:10:22 +0530

    Update msg_automation.py

--- msg_automation.py
+++ msg_automation.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client 
  
-account_sid = 'AC50166d95b8c1149b724f46c96b2515f3' 
-auth_token = 'ecf04d39a8dad375200ae4fb728e4b8a' 
+account_sid = 'AC50166s05b8c1149b724f46c96b2515f3' 
+auth_token = 'ecf04d3988dad375288ae4fb728e4b8a' 
 client = Client(account_sid, auth_token) 
 
 def send_msg():
     message = client.messages.create( 
                                 from_='whatsapp:+14155238886',  
                                 body='Good Morning buddy 👍',      
-                                to='whatsapp:+919123233155' 
+                                to='whatsapp:+919999999999' 
                             ) 
     
     print(message.sid)
